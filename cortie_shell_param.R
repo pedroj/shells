7228f73b0f5d06466e94824269be3c26cfe793d7
Pedro Jordano <pedroj@me.com>
2022-04-17 01:42:08 +0200

    Added shell; corrected elapsed time info.
    Signed-off-by: Pedro Jordano <pedroj@me.com>

--- cortie_shell_param.R
+++ cortie_shell_param.R
@@ -12,31 +12,32 @@ library(Rcpp)
 # Parameters are in file shell_parameters_table1_Cortie.R
 
 #-----------------------------------------------------------------------------
+Sys.time()
 TIME <- Sys.time()
 #-----------------------------------------------------------------------------
 
-# Turritela
+# Lyria
 n_s <- 650L
 n_t <- 2000L
 n <- 1000
-alpha <- 88.9
-beta <- 4
-phi <- 55
+alpha <- 83.9
+beta <- -19
+phi <- 45
 mu <- 0
 Omega <- 0
-s_min <- -267
-s_max <- 39
-A <- 22.2
-a <- 1.3
-b <- 1.5
+s_min <- -51
+s_max <- 9
+A <- 50
+a <- 40
+b <- 14
 P <- 0
-W_1 <- 1
-W_2 <- 1
-N <- 0
-L <- 0
+W_1 <- 6
+W_2 <- 27
+N <- 8
+L <- 4
 D <- 1
 theta_start <- 0
-theta_end <- 30*pi
+theta_end <- 14.2*pi
 
 # Generate data
 df <- mollusc(n_s = n_s, n_t = n_t,
@@ -48,16 +49,16 @@ df <- mollusc(n_s = n_s, n_t = n_t,
 
 # Create plot
 p <- ggplot() +
-  geom_point(aes(x, z), df, size = 0.03, alpha = 0.3, color= "#812527") +
-  geom_path(aes(x, z), df, size = 0.03, alpha = 0.3, color= "#812527") +
+  geom_point(aes(x, z), df, size = 0.03, alpha = 0.3, color= "#662E2E") +
+  geom_path(aes(x, z), df, size = 0.03, alpha = 0.3, color= "#662E2E") +
   coord_equal() +
   theme_blankcanvas(margin_cm = 0) +
-  theme(plot.background = element_rect(fill = "#E8D0D1"))
+  theme(plot.background = element_rect(fill = "#FFD1D1"))
 
 # p
 
 # Save plot
-ggsave("./images/turritela02#812527_1.png", p, 
+ggsave("./images/lyria02##662E2E_1.png", p, 
 	   width = 40, height = 40, units = "cm", dpi= 300)
 
 #-----------------------------------------------------------------------------
@@ -79,9 +80,10 @@ p <- ggplot() +
 	theme_blankcanvas(margin_cm = 0.5)
 p
 
-ggsave("./images/turritela02#812527_3.png", 
+ggsave("./images/lyria02##662E2E_3.png", 
 	   width = 80, height = 80, units = "cm", dpi= 300)
 
 #-----------------------------------------------------------------------------
+Sys.time()
 Sys.time() - TIME
 #-----------------------------------------------------------------------------
